bc62f96eaa4d57c27989e41797a2badbd8064efe
Adib Err <adib.err@hotmail.com>
2019-01-22 01:19:55 +0000

    Impl pull properties from external properties file.

--- src/main/java/com/adiberrifai/springbootplayground/SpringBootPlaygroundApplication.java
+++ src/main/java/com/adiberrifai/springbootplayground/SpringBootPlaygroundApplication.java
@@ -1,13 +1,20 @@
 package com.adiberrifai.springbootplayground;
 
+import com.adiberrifai.springbootplayground.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBootPlaygroundApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootPlaygroundApplication.class, args);
+
+        ApplicationContext ctx = SpringApplication.run(SpringBootPlaygroundApplication.class, args);
+
+        FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+
+        System.out.println(fakeDataSource);
     }
 
 }
